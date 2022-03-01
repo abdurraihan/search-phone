9b7d77b87acf815e63a549b8402225c61ef481b9
abdurraihan <abdurraihan898@gmail.com>
2022-03-01 21:19:15 +0600

    3rd time

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hellow");
+
+const  searchPhone = () =>{
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+   
+    searchField.value ="";
+
+    const url =`https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+
+    fetch(url) 
+    .then(res => res.json())
+    .then(data =>  displayResult(data.data)); 
+
+}
